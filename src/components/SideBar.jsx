7724d5628f499934d2ebb44bc98c1d0b46ec14ab
Sandi Sopian <shunasand73@gmail.com>
2023-02-16 21:35:30 +0700

    feat(components): add structure folder for more clean files

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
+      <div>This is sidebar</div>
+    </div>
+  );
+};
+
+export default SideBar;
