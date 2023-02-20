603a1309f2a9dff538497bbc1d831301bedd5702
Sandi Sopian <shunasand73@gmail.com>
2023-02-20 20:59:52 +0700

    feat(pokemondata.js): added origin type and origin background

--- src/data/pokemonData.js
+++ src/data/pokemonData.js
@@ -1,10 +1,86 @@
-export const firePokemon = [
+import MCX from "../assets/images/Mega-Charizard-X.png";
+
+export const PokemonData = [
   {
     id: "1",
+    name: "Mega Charizard X",
+    position: "State",
+    role: ["Output", "Buring"],
+    roleDescription: "Fire Team's Status-type Pokemon",
+    origin: "fire",
+    bgOrigin: "fire2",
+    types: ["Fire", "Dragon"],
+    rarity: "SSR",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+    flash: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+    extremeTraining: {
+      stage: "5",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+    },
+    source: ["Island IV", "Island V", "Shop", "World Gym"],
+    img: MCX,
+    evolutions: {
+      img: "",
+      name: "Charizard",
+      type: "fire",
+    },
+    baseStats: {
+      Hp: "124",
+      HpDesc: "S",
+      ATK: "145",
+      ATKDesc: "SS",
+      DEF: "100",
+      DEFDesc: "A",
+      SpATK: "90",
+      SpATKDesc: "A",
+      SpDef: "91",
+      SpDefDesc: "A",
+      Speed: "100",
+      SpeedDesc: "A",
+    },
+    skillInfo: {
+      ohko: {
+        name: "Flamethrower",
+        energy: "3",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      overheat: {
+        name: "Dragon Claw",
+        energy: "1",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      normal: {
+        name: "Dragon Tail",
+        energy: "0",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+    },
+    Talent: {
+      floor2: {
+        name: "Floor 2 - Talent Skill (3)",
+        energy: "3",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      floor3a: {
+        name: "Floor 3 - Talent Skill (1)",
+        energy: "1",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      floor3b: {
+        name: "Floor 3 - Talent Skill (2)",
+        energy: "0",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+    },
+  },
+  {
+    id: "2",
     name: "Mega Charizard X",
     position: "state",
     role: ["Output", "Buring"],
     roleDescription: "Fire Team's Status-type Pokemon",
+    origin: "water",
+    bgOrigin: "water2",
     types: ["Fire", "Dragon"],
     rarity: "SSR",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
