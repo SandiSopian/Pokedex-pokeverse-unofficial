f0f367f1e973e37292093f913d4aa93772884c92
Sandi Sopian <shunasand73@gmail.com>
2023-02-20 20:58:32 +0700

    feat(pokecard.jsx): added any style and layout for cards

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
-import { firePokemon } from "../data/pokemonData";
+import { PokemonData } from "../data/pokemonData";
 
 const PokeCard = () => {
-  const [FirePokemon, setFirePokemon] = useState(firePokemon);
+  const [PokemonDt, setPokemonDt] = useState(PokemonData);
 
   return (
-    <section className="bg-gray-100">
-      {FirePokemon.map((pokemon) => (
-        <div className="border w-[300px] flex ">
-          <div>
-            <div>{pokemon.name}</div>
-            <div>{pokemon.position}</div>
-            <div>{pokemon.rarity}</div>
-            <div>{pokemon.types}</div>
-            <div></div>
-            <img src={pokemon.img} />
+    <section className="grid gap-4">
+      {PokemonDt.map((pokemon, index) => (
+        <div key={index} className={`border rounded-md w-[300px] flex flex-row bg-${pokemon.origin}`}>
+          <div className="w-7/12 mr-1">
+            <div className="container p-3 flex flex-col gap-1">
+              <div className="border rounded-md">{pokemon.name}</div>
+              <div className="flex gap-12">
+                <div className="border rounded-md px-2 py-1">{pokemon.position}</div>
+                <div className="border rounded-full p-1">{pokemon.rarity}</div>
+              </div>
+              <div className="flex gap-6">
+                {pokemon.types.map((type, index) => {
+                  return (
+                    <div key={index} className="border rounded-md px-2 py-1">
+                      {type}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+          <div className={`w-5/12 flex items-center justify-center rounded-l-full bg-${pokemon.bgOrigin}`}>
+            <img src={pokemon.img} alt={pokemon.name} className="pl-6" />
           </div>
         </div>
       ))}
