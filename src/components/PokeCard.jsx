959b22f1bc0bc36207c0c37da123106cf6dd7bc9
Sandi Sopian <shunasand73@gmail.com>
2023-02-20 22:12:51 +0700

    fix(pokecard.jsx): added new variable for declare classes

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -3,6 +3,44 @@ import { PokemonData } from "../data/pokemonData";
 
 const PokeCard = () => {
   const [PokemonDt, setPokemonDt] = useState(PokemonData);
+  let colors = [
+    "bg-normal" == "bg-normal",
+    "bg-normal2" == "bg-normal2",
+    "bg-fire" == "bg-fire",
+    "bg-fire2" == "bgfire2",
+    "bg-water" == "bg-water",
+    "bg-water2 == bg-water2",
+    "bg-electric" == "bg-electric",
+    "bg-electric2" == "bg-electric2",
+    "bg-grass" == "bg-grass",
+    "bg-grass2" == "bg-grass2",
+    "bg-ice" == "bg-ice",
+    "bg-ice2" == "bg-ice2",
+    "bg-fighting" == "bg-fighting",
+    "bg-fighting2" == "bg-fighting2",
+    "bg-poison" == "bg-poison",
+    "bg-poison2" == "bg-poison2",
+    "bg-ground" == "bg-ground",
+    "bg-ground2" == "bg-ground2",
+    "bg-flying" == "bg-flying",
+    "bg-flying2" == "bg-flying2",
+    "bg-psychic" == "bg-psychic",
+    "bg-psychic2" == "bg-psychic2",
+    "bg-bug" == "bg-bug",
+    "bg-bug2" == "bg-bug2",
+    "bg-rock" == "bg-rock",
+    "bg-rock2" == "bg-rock2",
+    "bg-ghost" == "bg-ghost",
+    "bg-ghost2" == "bg-ghost2",
+    "bg-dragon" == "bg-dragon",
+    "bg-dragon2" == "bg-dragon2",
+    "bg-dark" == "bg-dark",
+    "bg-dark2" == "bg-dark2",
+    "bg-steel" == "bg-steel",
+    "bg-steel2" == "bg-steel2",
+    "bg-fairy" == "bg-fairy",
+    "bg-fairy2" == "bg-fairy2",
+  ];
 
   return (
     <section className="grid gap-4">
@@ -10,15 +48,15 @@ const PokeCard = () => {
         <div key={index} className={`border rounded-md w-[300px] flex flex-row bg-${pokemon.origin}`}>
           <div className="w-7/12 mr-1">
             <div className="container p-3 flex flex-col gap-1">
-              <div className="border rounded-md">{pokemon.name}</div>
-              <div className="flex gap-12">
-                <div className="border rounded-md px-2 py-1">{pokemon.position}</div>
-                <div className="border rounded-full p-1">{pokemon.rarity}</div>
+              <div className="rounded-md">{pokemon.name}</div>
+              <div className="flex justify-between gap-2">
+                <div className="border border-gray-400 rounded-md px-6 p-1">{pokemon.position}</div>
+                <div className="border-2 border-gray-400 rounded-full p-1">{pokemon.rarity}</div>
               </div>
-              <div className="flex gap-6">
+              <div className="flex justify-between gap-2">
                 {pokemon.types.map((type, index) => {
                   return (
-                    <div key={index} className="border rounded-md px-2 py-1">
+                    <div key={index} className="border border-gray-400 rounded-md px-4">
                       {type}
                     </div>
                   );
--- src/data/pokemonData.js
+++ src/data/pokemonData.js
@@ -1,6 +1,7 @@
 import MCX from "../assets/images/Mega-Charizard-X.png";
 
 export const PokemonData = [
+  // id:1
   {
     id: "1",
     name: "Mega Charizard X",
@@ -73,6 +74,7 @@ export const PokemonData = [
       },
     },
   },
+  // id:2
   {
     id: "2",
     name: "Mega Charizard X",
@@ -81,6 +83,151 @@ export const PokemonData = [
     roleDescription: "Fire Team's Status-type Pokemon",
     origin: "water",
     bgOrigin: "water2",
+    types: ["Flying", "Ground"],
+    rarity: "SSR",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+    flash: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+    extremeTraining: {
+      stage: "5",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+    },
+    source: ["Island IV", "Island V", "Shop", "World Gym"],
+    img: "",
+    evolutions: {
+      img: "",
+      name: "Charizard",
+      type: "fire",
+    },
+    baseStats: {
+      Hp: "124",
+      HpDesc: "S",
+      ATK: "145",
+      ATKDesc: "SS",
+      DEF: "100",
+      DEFDesc: "A",
+      SpATK: "90",
+      SpATKDesc: "A",
+      SpDef: "91",
+      SpDefDesc: "A",
+      Speed: "100",
+      SpeedDesc: "A",
+    },
+    skillInfo: {
+      ohko: {
+        name: "Flamethrower",
+        energy: "3",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      overheat: {
+        name: "Dragon Claw",
+        energy: "1",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      normal: {
+        name: "Dragon Tail",
+        energy: "0",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+    },
+    Talent: {
+      floor2: {
+        name: "Floor 2 - Talent Skill (3)",
+        energy: "3",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      floor3a: {
+        name: "Floor 3 - Talent Skill (1)",
+        energy: "1",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      floor3b: {
+        name: "Floor 3 - Talent Skill (2)",
+        energy: "0",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+    },
+  },
+  // id:3
+  {
+    id: "3",
+    name: "Mega Charizard X",
+    position: "state",
+    role: ["Output", "Buring"],
+    roleDescription: "Fire Team's Status-type Pokemon",
+    origin: "poison",
+    bgOrigin: "poison2",
+    types: ["Fire", "Dragon"],
+    rarity: "SSR",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+    flash: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+    extremeTraining: {
+      stage: "5",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+    },
+    source: ["Island IV", "Island V", "Shop", "World Gym"],
+    img: "",
+    evolutions: {
+      img: "",
+      name: "Charizard",
+      type: "fire",
+    },
+    baseStats: {
+      Hp: "124",
+      HpDesc: "S",
+      ATK: "145",
+      ATKDesc: "SS",
+      DEF: "100",
+      DEFDesc: "A",
+      SpATK: "90",
+      SpATKDesc: "A",
+      SpDef: "91",
+      SpDefDesc: "A",
+      Speed: "100",
+      SpeedDesc: "A",
+    },
+    skillInfo: {
+      ohko: {
+        name: "Flamethrower",
+        energy: "3",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      overheat: {
+        name: "Dragon Claw",
+        energy: "1",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      normal: {
+        name: "Dragon Tail",
+        energy: "0",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+    },
+    Talent: {
+      floor2: {
+        name: "Floor 2 - Talent Skill (3)",
+        energy: "3",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      floor3a: {
+        name: "Floor 3 - Talent Skill (1)",
+        energy: "1",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+      floor3b: {
+        name: "Floor 3 - Talent Skill (2)",
+        energy: "0",
+        skillDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
+      },
+    },
+  }, // id:4
+  {
+    id: "4",
+    name: "Mega Charizard X",
+    position: "state",
+    role: ["Output", "Buring"],
+    roleDescription: "Fire Team's Status-type Pokemon",
+    origin: "dragon",
+    bgOrigin: "dragon2",
     types: ["Fire", "Dragon"],
     rarity: "SSR",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aperiam officiis magni nam ut blanditiis harum doloremque ipsum perspiciatis earum",
