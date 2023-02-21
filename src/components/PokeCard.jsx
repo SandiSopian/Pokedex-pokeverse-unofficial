ee6700888fa8a44de0a5d0806d34b35205ed64f9
Sandi Sopian <shunasand73@gmail.com>
2023-02-21 23:02:45 +0700

    feat(pokecard.jsx): added select for filtering origin of pokemon

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { PokemonData } from "../data/pokemonData";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const PokeCard = () => {
   const [PokemonDt, setPokemonDt] = useState(PokemonData);
+  const [parent] = useAutoAnimate();
   let colors = [
     "bg-normal" == "bg-normal",
     "bg-normal2" == "bg-normal2",
@@ -42,35 +44,61 @@ const PokeCard = () => {
     "bg-fairy2" == "bg-fairy2",
   ];
 
+  const filter = (origin) => {
+    setPokemonDt(PokemonData.filter((pokeOrigin) => pokeOrigin.origin === origin));
+  };
+
   return (
     <section className="grid gap-4">
-      {PokemonDt.map((pokemon, index) => (
-        <div key={index} className={`border rounded-md w-[300px] flex flex-row bg-${pokemon.origin}`}>
-          <div className="w-7/12 mr-1">
-            <div className="container p-3 flex flex-col gap-1">
-              <div className="rounded-md">{pokemon.name}</div>
-              <div className="flex justify-between gap-2">
-                <div className="border border-gray-400 rounded-md px-6 p-1">{pokemon.position}</div>
-                <div className="border-2 border-gray-400 rounded-full p-1">{pokemon.rarity}</div>
-              </div>
-              <div className="flex justify-between gap-2">
-                {pokemon.types.map((type, index) => {
-                  return (
-                    <div key={index} className="border border-gray-400 rounded-md px-4">
-                      {type}
-                    </div>
-                  );
-                })}
+      {/* Searching Data */}
+
+      {/* Filtering Data */}
+      <div>
+        Select Type:
+        <select name="" subject="">
+          <option value="" selected="selected" onClick={() => setPokemonDt(PokemonData)}>
+            All
+          </option>
+          <option value="" selected="selected" onClick={() => filter("steel")}>
+            Steel
+          </option>
+          <option value="" selected="selected" onClick={() => filter("fire")}>
+            Fire
+          </option>
+        </select>
+      </div>
+
+      {/* Sorting Data */}
+
+      {/* Mapping Data */}
+      <div ref={parent}>
+        {PokemonDt.map((pokemon, index) => (
+          <div key={index} className={`border rounded-md w-[300px] flex flex-row bg-${pokemon.origin}`}>
+            <div className="w-7/12 mr-1">
+              <div className="container p-3 flex flex-col gap-1">
+                <div className="rounded-md">{pokemon.name}</div>
+                <div className="flex justify-between gap-2">
+                  <div className="border border-gray-400 rounded-md px-6 p-1">{pokemon.position}</div>
+                  <div className="border-2 border-gray-400 rounded-full p-1">{pokemon.rarity}</div>
+                </div>
+                <div className="flex justify-between gap-2">
+                  {pokemon.types.map((type, index) => {
+                    return (
+                      <div key={index} className="border border-gray-400 rounded-md px-4">
+                        {type}
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
+            <div className={`w-5/12 flex items-center justify-center rounded-l-full bg-${pokemon.bgOrigin}`}>
+              <img src={pokemon.img} alt={pokemon.name} className="pl-6" />
+            </div>
           </div>
-          <div className={`w-5/12 flex items-center justify-center rounded-l-full bg-${pokemon.bgOrigin}`}>
-            <img src={pokemon.img} alt={pokemon.name} className="pl-6" />
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 };
-
 export default PokeCard;
